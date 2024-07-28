b84e68a39547d812538f73e21fb446f0c105bf85
Daniel Glöckner <daniel-gl@gmx.net>
2024-07-28 15:23:39 +0200

    ffmpeg: update to 7.0.1

--- src/ffmpeg.mk
+++ src/ffmpeg.mk
@@ -3,8 +3,8 @@
 PKG             := ffmpeg
 $(PKG)_WEBSITE  := https://ffmpeg.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.0
-$(PKG)_CHECKSUM := 57be87c22d9b49c112b6d24bc67d42508660e6b718b3db89c44e47e289137082
+$(PKG)_VERSION  := 7.0.1
+$(PKG)_CHECKSUM := bce9eeb0f17ef8982390b1f37711a61b4290dc8c2a0c1a37b5857e85bfb0e4ff
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://ffmpeg.org/releases/$($(PKG)_FILE)
